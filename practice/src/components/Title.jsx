02079e31f4e6cc0b5808400da0ca9038aa9cc2ce
DM_churey <d.chureev2002@gmail.com>
2022-07-13 17:09:12 +0300

    tite

--- practice/src/components/Title.jsx
+++ practice/src/components/Title.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import "../App.css";
+import { useState } from "react";
+import React from 'react';
 
-const Title = () => {
-  return (
-    <div>Title</div>
-  )
+const Title = (props) => {
+
+  const [value, setValue] = useState(0);
+
+  return <div>
+    <div>
+      {value}
+    </div>
+    <button onClick={()=>{
+      setValue((value) => (value += 1));
+    }}>Поменять</button>
+  </div>
 }
 
 export default Title
